bc009db8d7038cc616bdf9c5a0b547007d70f494
Giruba Beulah SE <gibeulah@microsoft.com>
2019-05-11 23:06:32 +0530

    Add project files.

---
+++ ReverseAnInteger/IntegerUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseAnInteger
+{
+    class IntegerUtility
+    {
+        public static int GetReverseOfANumber(int number) {
+            if (number < 10) {
+                return number;
+            }
+            try
+            {
+                String result = "";
+                while (number != 0)
+                {
+                    int quotient = number / 10;
+                    int remainder = number % 10;
+                    number = quotient;
+                    result = result + remainder.ToString();
+                }
+                number = int.Parse(result.Trim());
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+            return number;
+        }
+    }
+}
---
+++ ReverseAnInteger/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseAnInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse an integer!");
+            Console.WriteLine("-------------------");
+
+            Console.WriteLine("Enter the number to be reversed");
+            try
+            {
+                Console.WriteLine("The reverse of the entered number is " +
+                IntegerUtility.GetReverseOfANumber(int.Parse(Console.ReadLine().Trim()))
+                );
+            }
+            catch (Exception exception) {
+                Console.WriteLine("Thrown exception is "+exception.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
